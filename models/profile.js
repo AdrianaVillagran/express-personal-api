54c602b9f9b0e13971925f3c98c3114d53c9790e
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-02 12:06:26 -0700

    hard coded profile data in seed.js and created profile schema in models/profile.js

---
+++ models/profile.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var PetSchema = new Schema({
+  name: String,
+  type: String,
+  breed: String,
+  alive: Boolean
+});
+
+var ProfileSchema = new Schema({
+  name: String,
+  github_link: String,
+  github_profile_image: String,
+  current_city: String,
+  pets: [PetSchema]
+});
+
+var Profile = mongoose.model('Profile', ProfileSchema);
+
+module.exports = Profile;
--- seed.js
+++ seed.js
@@ -1,15 +1,35 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var myProfile = {
+                name: "Adriana Villagran",
+                github_link: "https://github.com/AdrianaVillagran",
+                github_profile_image: "https://avatars3.githubusercontent.com/u/17095774?v=3&s=460",
+                current_city: "San Francisco",
+                pets:  [
+                  {
+                   name: "India",
+                   type: "Dog",
+                   breed: "Boston Terrier",
+                   alive: true
+                  },
+                  {
+                   name: "Rascal",
+                   type: "Cat",
+                   breed: "Domestic Short Hair",
+                   alive: false
+                  }]
+              };
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+
+db.Profile.create(myProfile, function(err, profile){
+  if (err){
+    return console.log("Error:", err);
+  }
+
+  console.log("Created new profile", profile._id);
+  process.exit(); // we're all done! Exit the program.
+});
