eafb16ad942438d79a11aab1f6e9464fa6560167
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-02 16:07:11 -0700

    defined routes in server.js

--- server.js
+++ server.js
@@ -45,11 +45,11 @@ app.get('/api', function api_index(req, res) {
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "GET", path: "/api/paintings", description: "Database of all my paintings and drawings"},
-      {method: "GET", path: "/api/paintings", description: "Database of all my paintings and drawings"},
-      {method: "POST", path: "/api/paintings", description: "Request a commission of artwork"},
-      {method: "PUT", path: "/api/paintings/:id", description: "Update a request for a commission"},
-      {method: "DELETE", path: "/api/paintings", description: "Delete a commission of artwork"}, // CHANGE ME
+      {method: "GET", path: "/api/artworks", description: "Database of all my artpieces"},
+      {method: "GET", path: "/api/artworks/:id", description: "Displays specific artwork"},
+      {method: "POST", path: "/api/artworks", description: "Request a commission of artwork"},
+      {method: "PUT", path: "/api/artworks/:id", description: "Update a request for a commission"},
+      {method: "DELETE", path: "/api/artworks/:id", description: "Delete a commission of artwork"}, // CHANGE ME
     ]
   });
 });
@@ -66,6 +66,71 @@ app.get('/api/profile', function(req, res) {
     res.status(200).json(profiles);
   });
 });
+
+app.get('/api/artworks', function(req, res) {
+  db.Artwork.find(function (err, artworks) {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).json(artworks);
+  });
+});
+
+app.get('/api/artworks/:id', function(req, res) {
+  var artworkId = req.params.id;
+  db.Artwork.findById(artworkId, function(err, foundArtwork) {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).json(foundArtwork);
+  });
+});
+
+app.post('/api/artworks', function(req, res) {
+  var newArtwork = new db.Artwork({
+    title: req.body.title,
+    medium: req.body.medium,
+    description: req.body.description,
+    image: req.body.image,
+    size: req.body.size,
+  });
+
+  newArtwork.save(function (err, savedArtwork) {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).json(savedArtwork);
+  });
+});
+
+app.delete('/api/artworks/:id', function(req, res) {
+  var artworkId = req.params.id;
+  db.Artwork.findOneAndRemove(artworkId, function(err, deletedArtwork) {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).json(deletedArtwork);
+  });
+});
+
+app.put('/api/artworks/:id', function(req, res) {
+  var artworkId = req.params.id;
+  db.Artwork.findById(artworkId, function(err, foundArtwork) {
+    if(err) {
+      console.log(err);
+    }
+    foundArtwork.title = req.body.title;
+    foundArtwork.medium = req.body.medium;
+    foundArtwork.description = req.body.description;
+    foundArtwork.image = req.body.image;
+    foundArtwork.size = req.body.size;
+
+    foundArtwork.save(function (err, updatedArtwork) {
+      res.status(200).json(updatedArtwork);
+    });
+  });
+});
+
 
 // listen on port 3000
 app.listen(process.env.PORT || 3000, function () {
